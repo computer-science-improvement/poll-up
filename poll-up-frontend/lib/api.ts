c0122a8a6c4ae079ac60e874a4467b7a536cc339
tupotavalentyn <gm.tupota.valik@gmail.com>
2023-11-05 04:51:47 +0200

    feature: configure api

--- poll-up-frontend/lib/api.ts
+++ poll-up-frontend/lib/api.ts
@@ -1,1 +1,9 @@
+import axios from 'axios';
+
 export const BASE_URL = 'https://1864-78-137-13-80.ngrok-free.app/api/v1';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export default api;
